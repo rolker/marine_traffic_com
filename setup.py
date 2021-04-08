6c1098f9c70355ea801be7fded39aa839039abf3
Roland Arsenault <roland@ccom.unh.edu>
2021-04-08 09:32:35 -0400

    Remove non-exsiting module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['marine_traffic_com'],
+    packages=[],
     package_dir={'': 'src'}
 )
 
